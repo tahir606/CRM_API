5ba79a3b2afdd8790e2181c4f497cf7944026e85
muhammadUsman625 <62797598+muhammadUsman625@users.noreply.github.com>
2020-08-14 23:38:09 -0700

    Update CRMApplication.java
    first change

--- src/main/java/com/example/CRM/CRMApplication.java
+++ src/main/java/com/example/CRM/CRMApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CRMApplication {
-
+//changing in crmapplication
 	public static void main(String[] args) {
 		SpringApplication.run(CRMApplication.class, args);
 	}
